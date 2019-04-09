bd7b4bdb6d3dd824ec590fa6e3520f85778bb53d
Sue <sueyoung.6311@gmail.com>
2019-04-08 20:14:24 -0400

    Initial commit

---
+++ src/main/java/com/bysue/springboot_17/Springboot17Application.java
@@ -0,0 +1,13 @@
+package com.bysue.springboot_17;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot17Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot17Application.class, args);
+	}
+
+}
